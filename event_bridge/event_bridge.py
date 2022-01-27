5a0dbe7ece9d6b580eb318d2f96603b98558fdc6
amumin <mohammed.abdul@orchids.edu.in>
2022-01-27 17:58:18 +0530

    Pushing first vesion of event_bridge package

---
+++ event_bridge/event_bridge.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import pdb
+
+class AWSEventBidge():
+    def __init__(self):
+        pass
+    def create_event(self):
+        print("creating event")
+
+def call_event_bridge(decorable_fun):
+    print("creating event bridge event")
+    return decorable_fun
---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+setuptools.setup(
+        name='aws_eb_util',
+        version='0.1',
+        description='AWS Event Bridge Package',
+        url='https://github.com/mmumin/tets_repo',
+        author='dummy',
+        install_requires=['psycopg2==2.9.3'],
+        author_email='mumin.mohammed@gmail.com',
+        packages=setuptools.find_packages(),
+        zip_safe=False
+        )
+
