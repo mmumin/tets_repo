ca2dd77aebc7cc88c45ff32a7a546afde138f860
amumin <mohammed.abdul@orchids.edu.in>
2022-01-31 23:12:28 +0530

    v06 of event broadcast package

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
 setuptools.setup(
         name='aws_eb_util',
-        version='0.0.6',
+        version='0.0.7',
         description='AWS Event Bridge Package',
         url='https://github.com/mmumin/tets_repo',
         author='dummy',
-        install_requires=['boto3'],
+        install_requires=['boto3==1.16.10', 'python-decouple==3.4'],
         author_email='mumin.mohammed@gmail.com',
         packages=setuptools.find_packages(),
         zip_safe=False
