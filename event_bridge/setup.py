9d9a9d048cfbaf3133a36a643b88863e14390896
amumin <mohammed.abdul@orchids.edu.in>
2022-02-04 08:45:15 +0530

    Moving seup.py inside

--- event_bridge/setup.py
+++ event_bridge/setup.py
@@ -1,7 +1,7 @@
 import setuptools
 setuptools.setup(
         name='aws_eb_util',
-        version='0.0.7',
+        version='0.0.8',
         description='AWS Event Bridge Package',
         url='https://github.com/mmumin/tets_repo',
         author='dummy',
